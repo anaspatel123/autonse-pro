e4cf7d4bb3130ffd47cac5519961bd6fb1996ed4
anaspatel123 <anaspatel584@gmail.com>
2026-09-07 16:41:32 +0530

    Update AutoNSE_Pro_app.py

--- AutoNSE_Pro_app.py
+++ AutoNSE_Pro_app.py
@@ -10,7 +10,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import yfinance as yf
-import pandas_ta as ta
+
 
 # ============================================================
 # AUTO NSE MOMENTUM SCANNER
